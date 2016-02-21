651abc4509c2663e2c011a0d632347bfea1dd77b
Romain Beaumont <romain.rom1@gmail.com>
2016-02-21 15:13:09 +0100

    flying-squid like organization

--- app.js
+++ app.js
@@ -1,79 +1,15 @@
-'use strict';
+#!/usr/bin/env node
 
-var mc = require('minecraft-classic-protocol');
-var zlib = require('zlib');
-var mca = require('minecraft-classic-anvil');
-var fs = require('fs');
+var mcServer=require("./");
 
-var options = { port: 25565 };
-var server = mc.createServer(options);
-var players = 0;
-var World = require("./world");
-var Vec3 = require("vec3").Vec3;
+var settings;
 
-server.on('login', function(client) {
-  players++;
-  console.log("someone connected!");
-  var addr = client.socket.remoteAddress + ':' + client.socket.remotePort;
+try {
+  settings = require('./config/settings');
+}
+catch(err) {
+  settings = require('./config/default-settings');
+}
 
-  client.on('end', function() {
-    console.log("someone left!");
-    players--;
-  });
+module.exports=mcServer.createMCServer(settings);
 
-  client.write('level_initialize', {});
-
-  var map=new World({x:256,y:64,z:256});
-  map.load(mca.readSync(fs.readFileSync(__dirname + '/level.dat')).blocks.$);
-
-  // for(let x=0;x<256;x++)
-  //   for(let z=0;z<256;z++)
-  //     for(let y=0;y<128;y++)
-  //     map.setBlock(new Vec3(x,y,z),3);
-
-  var compressedMap=zlib.gzipSync(map.dump());
-
-  for(var i=0;i<compressedMap.length;i+=1024) {
-    client.write('level_data_chunk', {
-      chunk_data: compressedMap.slice(i, Math.min(i + 1024, compressedMap.length)),
-      percent_complete: i==0 ? 0 : Math.ceil(i/compressedMap.length * 100)
-    });
-  }
-
-  client.write('level_finalize', {
-    x_size: 256,
-    y_size: 64,
-    z_size: 256
-  });
-
-  client.write('spawn_player', {
-    player_id: -1,
-    player_name: "UserXYZ",
-    x: 1 * 32,
-    y: 32 * 32,
-    z: 1 * 32,
-    yaw: 0,
-    pitch: 0
-  });
-
-  client.write('message', {
-    player_id: -1,
-    message: 'UserXYZ joined the game'
-  });
-
-  client.on('message', function(packet) {
-    client.write('message', {
-      player_id: -1,
-      message: packet.message
-    });
-  });
-
-});
-
-server.on('error', function(error) {
-  console.log('Oops! Something went wrong, ', error);
-});
-
-server.on('listening', function() {
-  console.log('Server running on port', server.socketServer.address().port + "!");
-});
---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const mc = require('minecraft-classic-protocol');
+const EventEmitter = require('events').EventEmitter;
+const path = require('path');
+const requireIndex = require('requireindex');
+
+
+function createMCServer(options) {
+  options = options || {};
+  const mcServer = new MCServer();
+  mcServer.connect(options);
+  return mcServer;
+}
+
+class MCServer extends EventEmitter {
+  constructor() {
+    super();
+    this._server = null;
+  }
+
+  connect(options) {
+    const plugins = requireIndex(path.join(__dirname, 'lib', 'plugins'));
+    this._server = mc.createServer(options);
+    Object.keys(plugins)
+      .filter(pluginName => plugins[pluginName].server!=undefined)
+      .forEach(pluginName => plugins[pluginName].server(this, options));
+    //if(options.logging == true) this.createLog();
+    this._server.on('error', error => this.emit('error',error));
+    this._server.on('listening', () => this.emit('listening',this._server.socketServer.address().port));
+    this.emit('asap');
+  }
+}
+
+
+module.exports = {
+  createMCServer:createMCServer
+};
