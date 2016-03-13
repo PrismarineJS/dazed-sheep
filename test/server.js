f38426be7a01a7d4e78474f40ffbd2941fcc18d6
mhsjlw <mhsjlw@users.noreply.github.com>
2016-03-13 12:22:06 -0400

    why circleci

--- test/server.js
+++ test/server.js
@@ -4,7 +4,7 @@ const net = require('net');
 const mc = require('minecraft-classic-protocol');
 
 describe("server", function() {
-  this.timeout(5000);
+  this.timeout(10000);
   let serv;
   let client;
   let loaded = false;
